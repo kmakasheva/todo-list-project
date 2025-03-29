8e92614aae1901a15fc3a03e16d8c6990d78d436
Kamila Makasheva <kmakasheva1@gmail.com>
2025-03-29 17:18:18 +0500

    Token in secrets

--- tests/settings.go
+++ tests/settings.go
@@ -1,7 +1,9 @@
 package tests
 
+import "os"
+
 var Port = 7540
 var DBFile = "../scheduler.db"
 var FullNextDate = true
 var Search = true
-var Token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NDMwODE1MjR9.2WHkwTBjXPgn64vZbNH_CVa_kwxLpPPPx0Z5ATdFZOk"
+var Token = os.Getenv("AUTH_TOKEN")
